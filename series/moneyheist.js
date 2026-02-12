a439f09c47721a9838927a695e252b7c5624ec15
yngofficial07-bit <yngofficial07@gmail.com>
2026-02-12 15:32:55 +0530

    Create moneyheist.js

---
+++ series/moneyheist.js
@@ -0,0 +1,21 @@
+/* series/moneyheist.js */
+const moneyHeistData = {
+    title: "Money Heist (hindi)",
+    poster: "https://m.media-amazon.com/images/M/MV_BNDJjNS00ZGMxLWFkYTAtZWRmYmFhZmU0NzZkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    description: "An unusual group of robbers attempt to carry out the most perfect robbery in Spanish history - stealing 2.4 billion euros from the Royal Mint of Spain.",
+    seasons: [
+        {
+            name: "Season 1",
+            episodes: [
+                { title: "Episode 1: Part 1", link: "https://www.youtube.com/embed/hMANIarjT50" },
+                { title: "Episode 2: Part 2", link: "https://www.youtube.com/embed/XXXXXX" }
+            ]
+        },
+        {
+            name: "Season 2",
+            episodes: [
+                { title: "Episode 1: New Plan", link: "https://www.youtube.com/embed/XXXXXX" }
+            ]
+        }
+    ]
+};
