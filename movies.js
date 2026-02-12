4f10c422f483d4f955b121f9826c8c22814122d0
yngofficial07-bit <yngofficial07@gmail.com>
2026-02-12 14:34:35 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,20 @@
+/* IndiPlex Movie Database */
+/* To add a new movie, just copy a { ... } block and paste it below the last one */
+
+const myMovies = [
+    {
+        title: "The Family Man",
+        poster: "https://m.media-amazon.com/images/M/MV_MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg",
+        link: "PASTE_LINK_HERE"
+    },
+    {
+        title: "Farzi",
+        poster: "https://m.media-amazon.com/images/M/MV_MV5BDRJiYjljMjctYjZlYy00ZjI3LWE0OWYtN2U3YTM3NGRjZGU0XkEyXkFqcGdeQXVyMTU0ODI1NTA2._V1_.jpg",
+        link: "PASTE_LINK_HERE"
+    },
+    {
+        title: "Pushpa: The Rise",
+        poster: "https://m.media-amazon.com/images/M/MV_MV5BMzFjMWUxN2QtOTlhOC00MzEwLTkxYzAtOWVjZTVlYjAyYTg4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+        link: "PASTE_LINK_HERE"
+    }
+];
