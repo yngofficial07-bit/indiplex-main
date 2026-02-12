acfa1983588e6133ef06456db76cf4e2067a7347
yngofficial07-bit <yngofficial07@gmail.com>
2026-02-12 15:27:19 +0530

    Create mirzapur.js

---
+++ series/mirzapur.js
@@ -0,0 +1,14 @@
+const mirzapurData = {
+    title: "Mirzapur",
+    poster: "https://m.media-amazon.com/images/M/MV_MV5BN2NlNGY0ZDgtNWJlOS00ZGRlLTlhYzAtZmZiYTM3NGE3NzY5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    description: "A shocking incident at a wedding procession ignites a series of events entangling the lives of two families in the lawless city of Mirzapur.",
+    seasons: [
+        {
+            name: "Season 1",
+            episodes: [
+                { title: "Episode 1: Jhandu", link: "https://www.youtube.com/embed/pS8S_S03m9E" },
+                { title: "Episode 2: Gooda", link: "https://www.youtube.com/embed/XXXXXX" }
+            ]
+        }
+    ]
+};
