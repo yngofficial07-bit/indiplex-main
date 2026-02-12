ec85b54b8d1168e113cb84f9088090a79768f207
yngofficial07-bit <yngofficial07@gmail.com>
2026-02-12 20:29:49 +0530

    Update mismatched.js

--- series/mismatched.js
+++ series/mismatched.js
@@ -1,1 +1,28 @@
-
+const mismatchedData = {
+    title: "Mismatched",
+    poster: "https://iili.io/q9UuY5F.png", 
+    description: "Mismatched is an Indian Hindi-language romantic drama series on Netflix based on Sandhya Menon’s novel When Dimple Met Rishi. The show follows teenagers Dimple and Rishi navigating college, friendships, and romance. ",
+    seasons: [
+        {
+            name: "Season 1",
+            episodes: [
+                { title: "Season 1 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25238/Money+Heist+S01+COMBINED+720p+NF+WEB-DL+DDP5.1+H+264+~VI.mkv?hash=AgADKA" }
+            ]
+        }, // <--- Season 1 yahan khatam, comma lagao
+        {
+            name: "Season 2",
+            episodes: [
+                { title: "Season 2 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25250/Money+Heist+S02+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD7Q" }
+            ]
+        }, // <--- Season 2 khatam, comma lagao
+        {
+            name: "Season 3",
+            episodes: [
+                { title: "Season 3 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25252/Money+Heist+S03+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD8A" }
+            ]
+        }
+      
+            
+        
+    ]
+};
