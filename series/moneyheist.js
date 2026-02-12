87acc5af6e5d31a5873206c2b5b01fd3bbdee120
yngofficial07-bit <yngofficial07@gmail.com>
2026-02-12 19:41:01 +0530

    Update moneyheist.js

--- series/moneyheist.js
+++ series/moneyheist.js
@@ -1,15 +1,39 @@
 const moneyHeistData = {
     title: "Money Heist",
-    // Tumhara Direct HD Link yahan hai:
     poster: "https://iili.io/q9UuY5F.png", 
     description: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan.",
     seasons: [
         {
-            name: "Season 1 Combined ",
+            name: "Season 1",
             episodes: [
-                { title: "watch online Season 1 Combined  / download ", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25238/Money+Heist+S01+COMBINED+720p+NF+WEB-DL+DDP5.1+H+264+~VI.mkv?hash=AgADKA" }
-                // Baaki episodes...
+                { title: "Season 1 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25238/Money+Heist+S01+COMBINED+720p+NF+WEB-DL+DDP5.1+H+264+~VI.mkv?hash=AgADKA" }
             ]
-        }
+        }, // <--- Season 1 yahan khatam, comma lagao
+        {
+            name: "Season 2",
+            episodes: [
+                { title: "Season 2 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25250/Money+Heist+S02+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD7Q" }
+            ]
+        }, // <--- Season 2 khatam, comma lagao
+        {
+            name: "Season 3",
+            episodes: [
+                { title: "Season 3 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25252/Money+Heist+S03+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD8A" }
+            ]
+        },
+        {
+            name: "Season 4",
+            episodes: [
+                { title: "Season 4 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25256/Money+Heist+S04+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD8w" }
+            ]
+         },
+         {
+            name: "Season 5",
+            episodes: [
+                { title: "Season 5 Combined [Hindi]", link: "https://streambotfu-bad36c8490c2.herokuapp.com/watch/25260/Money+Heist+S05+COMBINED+720p+NF+WEB-DL+AAC5.1+AV1+~VI%C5%82%C5%82.mkv?hash=AgAD9A" }
+            ]
+         }
+            
+        
     ]
 };
